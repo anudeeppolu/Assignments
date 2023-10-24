13f6f9faf8f35f151f04aaec6e0066e5f0986e0a
anudeeppolu <anudeeppolu@gmail.com>
2023-10-24 09:52:34 +0530

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import  centimelogo from "../Images/centimelogo.png"
+const Header = () => {
+  return (
+    <header>
+      <img src={centimelogo} alt="Centime Logo" />
+    </header>
+  );
+};
+
+export default Header;
